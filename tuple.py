cdc2d5dd532870d6606f768ceb4d95737b2903bf
Ali Mansoori <alimansoori824@gmail.com>
2026-04-08 21:41:38 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,20 @@
+# tuple is
+# collection of element
+# enclosed with ()
+# comma (,) seperated elements
+# ordered collection
+# indexing support
+# slicing supported
+# duplcate allowed
+# immutable in nature
+
+# only methods which doesnt change the structure are applicable in tuple
+# index()
+# count()
+
+a=eval(input("enter tuple"))
+print(a)
+print(a.index(3))
+print(a.count(5))#counts how many times a value is occuring
+
+# __ means magic methods or dinder method
