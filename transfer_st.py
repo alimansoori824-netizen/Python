ea4008c075aba6aabbb77be44e1d7c1272d3c549
Ali Mansoori <alimansoori824@gmail.com>
2026-05-13 23:56:51 +0530

    transfer st

---
+++ transfer_st.py
@@ -0,0 +1,10 @@
+#Transfer statement
+# continue - skip current iteration
+# break - terminate current loop
+# pass - skip current block
+
+for i in range(5):
+    if i==0:
+     continue #skips value
+     break # breaks loop
+     pass # skip current block
